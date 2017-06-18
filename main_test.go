6da2cea7346dfd083446f233314263ee59d6ba83
István Gazsi <istvan.gazsi@theag3nt.com>
2017-06-18 18:25:02 +0200

    Rename subtests and temporary variables

--- main_test.go
+++ main_test.go
@@ -14,28 +14,29 @@ func TestValidateArgs(t *testing.T) {
 		wantPorts string
 		wantErr   bool
 	}{
-		{"v4Localhost_singlePort", "127.0.0.1,8080", "127.0.0.1", "8080", false},
-		{"v4Localhost_multiPort", "127.0.0.1,8080,8081", "127.0.0.1", "8080,8081", false},
-		{"v4Internal_singlePort", "192.168.0.1,8080", "192.168.0.1", "8080", false},
-		{"v4Internal_multiPort", "192.168.0.1,8080,8081", "192.168.0.1", "8080,8081", false},
-		{"v6Localhost_singlePort", "::1,8080", "[::1]", "8080", false},
-		{"v6Localhost_multiPort", "::1,8080,8081", "[::1]", "8080,8081", false},
-		{"v6Internal_singlePort", "fd30:3fac:747b::1,8080", "[fd30:3fac:747b::1]", "8080", false},
-		{"v6Internal_multiPort", "fd30:3fac:747b::1,8080,8081", "[fd30:3fac:747b::1]", "8080,8081", false},
-		{"v4AllIfaces_singlePort", "0.0.0.0,8080", "0.0.0.0", "8080", false},
-		{"v4AllIfaces_multiPort", "0.0.0.0,8080,8081", "0.0.0.0", "8080,8081", false},
-		{"v6AllIfaces_singlePort", "::,8080", "[::]", "8080", false},
-		{"v6AllIfaces_multiPort", "::,8080,8081", "[::]", "8080,8081", false},
-		{"noIfaces_singlePort", "8080", "0.0.0.0", "8080", false},
-		{"noIfaces_multiPort", "8080,8081", "0.0.0.0", "8080,8081", false},
-		{"negative_tooFewArgs", "", "", "", true},
-		{"negative_invalidIPRange", "123.456.789", "", "", true},
-		{"negative_invalidIPDomain", "example.com", "", "", true},
-		{"negative_invalidPortRange", "127.0.0.1,99999", "", "", true},
-		{"negative_invalidPortName", "127.0.0.1,http", "", "", true},
+		{"Local/Single/v4", "127.0.0.1,8080", "127.0.0.1", "8080", false},
+		{"Local/Multiple/v4", "127.0.0.1,8080,8081", "127.0.0.1", "8080,8081", false},
+		{"Private/Single/v4", "192.168.0.1,8080", "192.168.0.1", "8080", false},
+		{"Private/Multiple/v4", "192.168.0.1,8080,8081", "192.168.0.1", "8080,8081", false},
+		{"Local/Single/v6", "::1,8080", "[::1]", "8080", false},
+		{"Local/Multiple/v6", "::1,8080,8081", "[::1]", "8080,8081", false},
+		{"Private/Single/v6", "fd30:3fac:747b::1,8080", "[fd30:3fac:747b::1]", "8080", false},
+		{"Private/Multiple/v6", "fd30:3fac:747b::1,8080,8081", "[fd30:3fac:747b::1]", "8080,8081", false},
+		{"Any/Single/v4", "0.0.0.0,8080", "0.0.0.0", "8080", false},
+		{"Any/Multiple/v4", "0.0.0.0,8080,8081", "0.0.0.0", "8080,8081", false},
+		{"Any/Single/v6", "::,8080", "[::]", "8080", false},
+		{"Any/Multiple/v6", "::,8080,8081", "[::]", "8080,8081", false},
+		{"None/Single", "8080", "0.0.0.0", "8080", false},
+		{"None/Multiple", "8080,8081", "0.0.0.0", "8080,8081", false},
+		{"Negative/TooFewArgs", "", "", "", true},
+		{"Negative/InvalidIPRange", "123.456.789", "", "", true},
+		{"Negative/InvalidIPDomain", "example.com", "", "", true},
+		{"Negative/InvalidPortRange", "127.0.0.1,99999", "", "", true},
+		{"Negative/InvalidPortName", "127.0.0.1,http", "", "", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Prepare arguments and expected results
 			args := strings.Split(tt.args, ",")
 			if tt.args == "" {
 				args = nil
@@ -45,8 +46,9 @@ func TestValidateArgs(t *testing.T) {
 				wantPorts = nil
 			}
 
-			gotIP, gotPorts, err := validateArgs(args)
+			ip, ports, err := validateArgs(args)
 
+			// Check the returned error
 			if (err != nil) != tt.wantErr {
 				expectErr := "expected"
 				if !tt.wantErr {
@@ -55,11 +57,13 @@ func TestValidateArgs(t *testing.T) {
 				t.Errorf("error is %s, got error: %v", expectErr, err)
 				return
 			}
-			if gotIP != tt.wantIP {
-				t.Errorf("invalid IP: got %q, want %q", gotIP, tt.wantIP)
+			// Check validated IP address
+			if ip != tt.wantIP {
+				t.Errorf("invalid IP: got %q, want %q", ip, tt.wantIP)
 			}
-			if !reflect.DeepEqual(gotPorts, wantPorts) {
-				t.Errorf("invalid ports: got %v, want %v", gotPorts, wantPorts)
+			// Check validated port list
+			if !reflect.DeepEqual(ports, wantPorts) {
+				t.Errorf("invalid ports: got %v, want %v", ports, wantPorts)
 			}
 		})
 	}
